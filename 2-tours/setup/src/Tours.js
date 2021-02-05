25edc5519e8c5d2e889b9e41c25a8bf4471a2855
Knight <mayanks365@gmail.com>
2021-02-05 23:38:15 +0530

    updating tours files

--- 2-tours/setup/src/Tours.js
+++ 2-tours/setup/src/Tours.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+
+
+const Tours = ( { tours, removeTour } ) => {
+  return (
+  <section> 
+    <div className="title">
+      <h2>ours tours</h2>
+      <div className="underline"></div>
+    </div>;
+    <div>
+      { tours.map( (tour) => {
+        // spread operator is being send so that Tour component
+        //  have access to object
+        // no prop needed
+        return <Tour key={tour.id} {...tour} removeTour={removeTour}></Tour>;
+
+      })}
+    </div>
+  </section>
+    )
 };
 
 export default Tours;
