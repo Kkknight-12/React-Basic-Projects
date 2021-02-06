04436e25041f81024e1b72267546ffe20d45dfec
Knight <mayanks365@gmail.com>
2021-02-06 19:45:34 +0530

    updating menu files

--- 5-menu/setup/src/App.js
+++ 5-menu/setup/src/App.js
@@ -3,8 +3,42 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+// set will return unique values
+const allCategories = ['all', ...new Set( items.map( (item) => {
+  return item.category
+}))]
+
 function App() {
-  return <h2>menu project setup</h2>;
+  
+  const [ menuItems, setMenuItems ] = useState(items);
+  const [ categories, setCategories ] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if( category === 'all' ){
+      setMenuItems(items);
+      return
+    }
+    const newItems = items.filter( (item) => {
+      return item.category === category 
+    })
+      setMenuItems(newItems);  
+  } 
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories 
+        categories= { categories }
+        filterItems = { filterItems }/>
+        <Menu items={ menuItems }/>
+      </section>
+    </main>
+  );
+
 }
 
 export default App;
